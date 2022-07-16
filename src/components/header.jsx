51715188c60f0dd3e7e977c5c400b5f0d64714c9
UntamedLotus <ruttikajadhav@gmail.com>
2022-07-16 21:00:39 +0530

    first commit

---
+++ src/components/header.jsx
@@ -0,0 +1,12 @@
+import "./header.scss";
+
+function Header() {
+  return (
+    <header className="header">
+      <img src="./images/meme-face.png" className="header--image" />
+      <h2 className="header--title">Meme Generator</h2>
+    </header>
+  );
+}
+
+export default Header;
